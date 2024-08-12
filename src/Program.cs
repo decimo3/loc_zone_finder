eae709f8da9af31a81f1273d78be23195d946618
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-08-12 09:45:07 -0300

    Added conditional for building the database or querying it

---
+++ src/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ZoneLocator;
+public static class Localizador
+{
+  public static void Main(String[] args)
+  {
+    using(var database = new Database())
+    {
+      if(args.Contains("--first-run"))
+      {
+        database.SetTemplates();
+        var templates = DataSeed.GetTemplatesFromFile();
+        database.AddTemplates(templates);
+        return;
+      }
+      else
+      {
+        var latitude = Double.Parse(args[0], System.Globalization.CultureInfo.InvariantCulture);
+        var longitude = Double.Parse(args[1], System.Globalization.CultureInfo.InvariantCulture);
+        var templates = database.GetTemplates(latitude, longitude, 1_000);
+        if(!templates.Any())
+        {
+          Console.WriteLine("Não foi encontrado nenhum equipamento próximo a sua localização!");
+          System.Environment.Exit(0);
+        }
+        Console.WriteLine(new String('#', 40));
+        foreach (var template in templates)
+        {
+          Console.WriteLine(template.ToString());
+          Console.WriteLine(new String('#', 40));
+        }
+      }
+    }
+  }
+}
